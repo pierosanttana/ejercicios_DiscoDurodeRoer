82cf1d0bd969db5ea7a271529ac3b529a27b0e37
pierosanttana <piero.santtana@gmail.com>
2024-06-11 15:06:49 +0200

    add cambio E14

--- Ejercicios_04_JavaBasicos/src/ejercicios/E14_ConvertirMayuMinu.java
+++ Ejercicios_04_JavaBasicos/src/ejercicios/E14_ConvertirMayuMinu.java
@@ -10,12 +10,45 @@ public class E14_ConvertirMayuMinu {
 	//el usuario lo pida y mostraremos el resultado por pantalla.
 
 		
+		
 		String texto = JOptionPane.showInputDialog(null,
 													"Ingresa una frase:",
 													"Convertir Mayuscula o Minuscula",
 													JOptionPane.INFORMATION_MESSAGE);
 		
+		/*
+		int eleccion = JOptionPane.showConfirmDialog(null, "¿Quieres que se pase a Mayusculas?", "Elección", JOptionPane.YES_NO_OPTION);
+		
+		char caracterActual = ' ';
+		String cadenaResultante = "";
+		final int DIFERENCIA = 32;
+		
 		boolean isMayus = false;
+		
+		isMayus = eleccion == JOptionPane.YES_OPTION;
+		
+		for(int i = 0; i < texto.length(); i++) {
+			caracterActual = texto.charAt(i);
+			
+			if(isMayus) {
+				if(caracterActual >= 97 && caracterActual <= 122) {
+					cadenaResultante += (char)(caracterActual - DIFERENCIA);
+				}else {
+					cadenaResultante += caracterActual;
+				}
+			}else {
+				if(caracterActual >= 65 && caracterActual <= 90) {
+					cadenaResultante += (char)(caracterActual + DIFERENCIA);
+				}else {
+					cadenaResultante += caracterActual;
+				}
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null, cadenaResultante, "Resultado", JOptionPane.INFORMATION_MESSAGE);
+		*/
+		
+		
 		
 		if(texto != null) {
 			String[] arrayOpcion = {"Mayuscula", "Minuscula"};
@@ -30,13 +63,18 @@ public class E14_ConvertirMayuMinu {
 			case 1:
 				texto = texto.toLowerCase();
 				break;
+			case 2:
+				break;
 			}
 			
-			
+		
 				JOptionPane.showMessageDialog(null, texto, "Resultado", JOptionPane.INFORMATION_MESSAGE);
 		}else {
 			JOptionPane.showMessageDialog(null, "No has ingresado ninguna frase.", "Resultado", JOptionPane.INFORMATION_MESSAGE);
 		}
+		
+		
+		
 	}
 
 }
