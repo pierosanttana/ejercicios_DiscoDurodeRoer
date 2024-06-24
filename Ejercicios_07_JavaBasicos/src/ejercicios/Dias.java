b4b1e530226af5b677493bb480b5d242884b78d9
pierosanttana <piero.santtana@gmail.com>
2024-06-24 16:08:38 +0200

    add E23 Enum Dias toString

--- Ejercicios_07_JavaBasicos/src/ejercicios/Dias.java
+++ Ejercicios_07_JavaBasicos/src/ejercicios/Dias.java
@@ -1,6 +1,27 @@
 package ejercicios;
 
 public enum Dias {
-	LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO;
-
+	LUNES(true), 
+	MARTES(true), 
+	MIERCOLES(true), 
+	JUEVES(true), 
+	VIERNES(true), 
+	SABADO(false), 
+	DOMINGO(false);
+	
+	private boolean laborable;
+	
+	private Dias(boolean laborable) {
+		this.laborable = laborable;
+	}
+	
+	@Override
+	public String toString() {
+		if(laborable) {
+			return "El dia "+ this.name() + " es laborable.";
+		}else {
+			return "El dia "+ this.name() + " no es laborable.";
+		}
+	}
+	
 }
--- Ejercicios_07_JavaBasicos/src/ejercicios/E23_EnumDias.java
+++ Ejercicios_07_JavaBasicos/src/ejercicios/E23_EnumDias.java
@@ -1,9 +1,22 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class E23_EnumDias {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 23. Modifica el anterior enum para indicar que es dia 
+		// laborable directamente (usar toString).
+		
+	Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Escribe un dia de la semana:");
+		
+		String sDia = scanner.nextLine();
+		
+		Dias eDia = Dias.valueOf(sDia.toUpperCase());
+		
+		System.out.println(eDia.toString());
 
 	}
 
